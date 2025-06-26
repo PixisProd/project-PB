d0eb7680fa2f1c90e4f584cc878f9472e9bf34bb
Sayidazim <xamzayew@gmail.com>
2025-06-26 01:36:33 -0700

    настроил роутинг

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,12 +5,14 @@ import './index.css';
 import Login from './pages/Login.tsx';
 import Register from './pages/Register.tsx';
 import Dashboard from './pages/Dashboard.tsx';
+import UserSettings from "./pages/UserSettings";
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
+        <Route path="/settings" element={<UserSettings />} />
         <Route path='/dashboard' element={<Dashboard />}/>
         <Route path='/register' element={<Register />}/>
         <Route path='/login' element={<Login />}/>
