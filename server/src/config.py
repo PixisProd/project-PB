64a4e280f505b9f3edd9c0a585b49c5f9a59456f
Jahongir <crazyout4@gmail.com>
2025-07-03 19:40:22 +0500

    bugfix(config): Fix config logic

--- server/src/config.py
+++ server/src/config.py
@@ -4,7 +4,12 @@ from pydantic import SecretStr, Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-class DatabaseSettings(BaseSettings):
+class ConfigBase(BaseSettings):
+    model_config = SettingsConfigDict(env_file='.env', extra='ignore')
+    
+
+
+class DatabaseSettings(ConfigBase):
     model_config = SettingsConfigDict(env_prefix='DB_')
     host: str
     user: SecretStr
@@ -12,7 +17,7 @@ class DatabaseSettings(BaseSettings):
     name: str
     test_name: str
 
-class JWTSettings(BaseSettings):
+class JWTSettings(ConfigBase):
     model_config = SettingsConfigDict(env_prefix='JWT_')
     secret_key: SecretStr
     algorithm: str
@@ -21,7 +26,7 @@ class JWTSettings(BaseSettings):
     refresh_token_cookie_name: str
     refresh_token_lifetime: timedelta = timedelta(days=7)
 
-class SubPlanSettings(BaseSettings):
+class SubPlanSettings(ConfigBase):
     model_config = SettingsConfigDict(env_prefix='SUB_')
     trial_max_prompts: int
     basic_max_prompts: int
@@ -33,12 +38,11 @@ class SubPlanSettings(BaseSettings):
     premium_versions_limit: int
     
 
-class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file='.env', extra='ignore')
+class Settings(ConfigBase):
     db: DatabaseSettings = Field(default_factory=DatabaseSettings)
     jwt: JWTSettings = Field(default_factory=JWTSettings)
     sub: SubPlanSettings = Field(default_factory=SubPlanSettings)
 
     ERROR_MESSAGE_KEY: str
-
+    
 settings = Settings()
